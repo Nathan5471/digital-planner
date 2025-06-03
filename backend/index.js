20033a76750eb79aac1da49486a1171d06d13f3e
Nathan Cloutier <nathanc4571@gmail.com>
2025-06-02 20:13:46 -0400

    Self hosting urls

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'https://digital-planner.onrender.com/', credentials: true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
+app.use(cors({ origin: 'https://digitalplanner.nathanc.me/', credentials: true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoute);
--- frontend/src/utils/AuthAPIHandler.js
+++ frontend/src/utils/AuthAPIHandler.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://digital-planner-backend.onrender.com/api/auth';
+const baseURL = 'https://digitalplannerapi.nathanc.me/api/auth';
 const api = axios.create({
     baseURL: baseURL,
     withCredentials: true,
